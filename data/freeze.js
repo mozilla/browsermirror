f3d51690b13dff7f6ab972a6e2675282abdda5b1
Ian Bicking <ianb@colorstudy.com>
2013-12-09 22:47:32 -0600

    Some refactoring for jshint, and jazz

--- data/freeze.js
+++ data/freeze.js
@@ -2,8 +2,8 @@ if (! window.Freeze) {
   var Freeze = {};
 }
 
-Freeze.makeId = function () {
-  return 'el' + (arguments.callee.counter++);
+Freeze.makeId = function makeId() {
+  return 'el' + (makeId.counter++);
 };
 
 // This makes it more sortable:
@@ -59,7 +59,7 @@ Freeze.skipElementsBadTags = {
 Freeze.skipElement = function (el) {
   /* true if this element should be skipped when sending to the mirror */
   var tag = el.tagName;
-  if (this.skipElementsBadTags[tag] || el.jsmirrorHide ||
+  if (Freeze.skipElementsBadTags[tag] || el.jsmirrorHide ||
       el.id == 'webSocketContainer') {
     return true;
   }
@@ -70,7 +70,7 @@ Freeze.skipElement = function (el) {
   // may still need to be displayed.
   if ((el.style && el.style.display == 'none') ||
       ((el.clientWidth === 0 && el.clientHeight === 0) &&
-       (! this.skipElementsOKEmpty[tag]) &&
+       (! Freeze.skipElementsOKEmpty[tag]) &&
        (! el.childNodes.length))) {
     return true;
   }
@@ -80,12 +80,12 @@ Freeze.skipElement = function (el) {
 Freeze.serializeDocument = function () {
   /* Serializes a complete document to JSON object */
   // FIXME: should I clear this.elements here?
-  var doc = unsafeWindow.document;
+  var doc = document;
   var result = {
     href: location.href,
-    htmlAttrs: this.serializeAttributes(doc.childNodes[0]),
-    head: this.serializeElement(doc.head),
-    body: this.serializeElement(doc.body),
+    htmlAttrs: Freeze.serializeAttributes(doc.childNodes[0]),
+    head: Freeze.serializeElement(doc.head),
+    body: Freeze.serializeElement(doc.body),
     hash: location.hash || ""
   };
   return result;
@@ -108,7 +108,7 @@ Freeze.serializeElement = function (el, includeHTML) {
      If includeHTML is true then an additional innerHTML item will be added to the end of each list
    */
   if (! el.jsmirrorId) {
-    this.trackElement(el);
+    Freeze.trackElement(el);
   }
   if (el.tagName == 'CANVAS') {
     return ['IMG', el.jsmirrorId, {src: el.toDataURL('image/png')}, []];
@@ -117,8 +117,9 @@ Freeze.serializeElement = function (el, includeHTML) {
   var children;
   if (el.tagName == 'IFRAME') {
     children = [];
+    var html;
     try {
-      var html = this.staticHTML(el.contentWindow.document.documentElement);
+      html = this.staticHTML(el.contentWindow.document.documentElement);
     } catch (e) {
       console.warn('Had to skip iframe for permission reasons:', e+'', 'src:', el.src);
       // A placeholder for the iframe:
@@ -253,12 +254,13 @@ Freeze.compareAttributes = function (attrs, el) {
 Freeze.compareAttributes2 = function (attrs, el) {
   var elAttrs = this.serializeAttributes(el);
   var result = true;
-  for (var i in attrs) {
+  var i;
+  for (i in attrs) {
     if (attrs[i] != elAttrs[i]) {
       result = false;
     }
   }
-  for (var i in elAttrs) {
+  for (i in elAttrs) {
     if (attrs[i] != elAttrs[i]) {
       result = false;
     }
@@ -312,12 +314,13 @@ Freeze.staticHTML = function (el) {
       if (name.substr(0, 2).toLowerCase() == "on") {
         continue;
       }
+      var value;
       if (name == 'src' && replSrc) {
-        var value = replSrc;
+        value = replSrc;
       } else if (name == "href" || name == "src" || name == "value") {
-        var value = el[name];
+        value = el[name];
       } else {
-        var value = attrs[i].nodeValue;
+        value = attrs[i].nodeValue;
       }
       // FIXME: should the name be quoted in any fashion?
       s += ' ' + name + '="' + this.htmlQuote(value) + '"';
@@ -341,10 +344,11 @@ Freeze.getAttributes = function (el) {
       if (name.substr(0, 2).toLowerCase() == "on") {
         continue;
       }
+      var value;
       if (name == "href" || name == "src" || name == "value") {
-        var value = el[name];
+        value = el[name];
       } else {
-        var value = attrs[i].nodeValue;
+        value = attrs[i].nodeValue;
       }
       result.push([name, value]);
     }
@@ -373,4 +377,401 @@ Freeze.staticChildren = function (el) {
     }
   }
   return s;
+};
+
+
+Freeze.diffDocuments = function (orig, current, commands) {
+  if (! current) {
+    throw 'Got bad current argument: ' + current;
+  }
+  if (commands === undefined) {
+    commands = [];
+  }
+  if (! current.jsmirrorId) {
+    console.warn("Got diffDocuments element without an id", current);
+    current.jsmirrorId = Freeze.makeId();
+    this.elements[current.jsmirrorId] = current;
+  }
+  var origTagName = orig[0];
+  var origId = orig[1];
+  var origAttrs = orig[2];
+  var origChildren = orig[3];
+  var origInnerHTML = orig[4];
+  if (origTagName != current.tagName) {
+    // We can't diff a tag that doesn't match
+    console.warn('got tag name change', origTagName, current.tagName);
+    return null;
+  }
+  if (! Freeze.compareAttributes(origAttrs, current)) {
+    commands.push(['attrs', current.jsmirrorId, Freeze.serializeAttributes(current)]);
+  }
+  if (origId != current.jsmirrorId) {
+    // This shouldn't happen
+    console.warn("Tag ids don't match", origId, current.jsmirrorId, current);
+    return null;
+  }
+  if (origInnerHTML !== undefined && current.innerHTML == origInnerHTML) {
+    // Deep comparison matches
+    return commands;
+  }
+  var curChildren = Freeze.normalChildren(current);
+  var curLength = curChildren.length;
+  var origLength = origChildren.length;
+  if (curLength === 1 && origLength === 1 &&
+      typeof curChildren[0] === "string" && typeof origChildren[0] === "string") {
+    // A special case of an element with just one string child
+    if (origChildren[0] !== curChildren[0]) {
+      commands.push(["replace_text", current.jsmirrorId, curChildren[0]]);
+    }
+    return commands;
+  }
+  var i;
+  var origPos = 0;
+  var curPos = 0;
+  while (origPos < origLength || curPos < curLength) {
+    // If two equal strings, walk forward
+    if (typeof origChildren[origPos] == "string" &&
+        origChildren[origPos] === curChildren[curPos]) {
+      origPos++;
+      curPos++;
+      continue;
+    }
+    var nextPos = Freeze.findNextMatch(origChildren, curChildren, origPos, curPos);
+    if (nextPos === null) {
+      // No more matches, so we need to add everything up to the end
+      nextPos = [origLength, curLength];
+    }
+    var origNext = nextPos[0];
+    var curNext = nextPos[1];
+    if (origPos < origNext) {
+      // We have to delete some orig children
+      if (origPos + 1 == origNext && typeof origChildren[origPos] == "string") {
+        // Only a string has changed
+        if (origNext >= origLength) {
+          commands.push(["delete_last_text", current.jsmirrorId]);
+        } else {
+          commands.push(["deletetext-", origChildren[origPos+1][1]]);
+        }
+      } else {
+        // Some elements have to be deleted
+        var startText = typeof origChildren[origPos] == "string";
+        for (i=origPos; i<origNext; i++) {
+          if (typeof origChildren[i] == "string") {
+            continue;
+            }
+          var command = "delete";
+          if (i == origPos+1 && startText) {
+            command += "-";
+          }
+          if (i+1 < origChildren && typeof origChildren[i+1] == "string") {
+            command += "+";
+          }
+          commands.push([command, origChildren[i][1]]);
+        }
+      }
+    }
+    if (curPos < curNext) {
+      // We have to insert some new children
+      var pushes = [];
+      for (i=curPos; i<curNext; i++) {
+        if (typeof curChildren[i] == "string") {
+          pushes.push(curChildren[i]);
+        } else {
+          if (! curChildren[i].jsmirrorId) {
+            curChildren[i].jsmirrorId = Freeze.makeId();
+            Freeze._elements[curChildren[i].jsmirrorId] = curChildren[i];
+          }
+          pushes.push(Freeze.serializeElement(curChildren[i]));
+        }
+      }
+      if (curChildren[curNext]) {
+        commands.push(["insert_before", curChildren[curNext], pushes]);
+      } else {
+        commands.push(["append_to", current.jsmirrorId, pushes]);
+      }
+    }
+    if (origChildren[origNext]) {
+      Freeze.diffDocuments(origChildren[origNext], curChildren[curNext], commands);
+    }
+    curPos = curNext+1;
+    origPos = origNext+1;
+  }
+  return commands;
+};
+
+Freeze.findNextMatch = function (origChildren, curChildren, origStart, curStart) {
+  if (origStart >= origChildren.length || curStart >= curChildren.length) {
+    return null;
+  }
+  while (typeof curChildren[curStart] == "string" ||
+         (! curChildren[curStart].jsmirrorId)) {
+    curStart++;
+    if (curStart >= curChildren.length) {
+      // There's nothing with an id
+      return null;
+    }
+  }
+  // First we see if we can find a match for curStart in origChildren
+  var check = origStart;
+  var checkId = curChildren[curStart].jsmirrorId;
+  if (! checkId) { // FIXME: why is this if statement here
+    while (check < origChildren.length) {
+      if (typeof origChildren[check] != "string" && checkId == origChildren[check][1]) {
+        return [check, curStart];
+      }
+      check++;
+    }
+  }
+  // We didn't find a match, so we'll try to find a match for the origStart in curChildren
+  // This should never really go more than one loop
+  while (typeof origChildren[origStart] == "string") {
+    origStart++;
+    if (origStart >= origChildren.length) {
+      // There's no more elements
+      return null;
+    }
+  }
+  checkId = origChildren[origStart][1];
+  check = curStart;
+  while (check < curChildren.length) {
+    if (typeof curChildren[check] != "string" &&
+        checkId == curChildren[check].jsmirrorId) {
+      return [origStart, check];
+    }
+    check++;
+  }
+  // Fell out of the loop - nothing matched, so we'll try later elements all around
+  return Freeze.findNextMatch(origChildren, curChildren, origStart+1, curStart+1);
+};
+
+Freeze.applyDiff = function (commands) {
+  var pushes, j, child;
+  for (var i=0; i<commands.length; i++) {
+    var command = commands[i];
+    var name = command[0];
+    var el = Freeze.getElement(command[1]);
+    if (! el) {
+      console.warn('Got diff command for element that does not exist', command);
+      continue;
+    }
+    if (name === 'attrs') {
+      Freeze.setAttributes(el, command[2]);
+    }
+    if (name === 'deletetext-' || name === 'delete-' || name == 'delete-+') {
+      while (el.previousSibling && el.previousSibling.nodeType == document.TEXT_NODE) {
+        el.parentNode.removeChild(el.previousSibling);
+      }
+    }
+    if (name === 'delete+') {
+      while (el.nextSibling && el.nextSibling.nodeType == document.TEXT_NODE) {
+        el.parentNode.removeChild(el.nextSibling);
+      }
+    }
+    if (name === 'delete' || name === 'delete-' || name === 'delete+' || name === 'delete-+') {
+      el.parentNode.removeChild(el);
+    }
+    if (name === 'delete_last_text') {
+      if (el.childNodes.length) {
+        var lastEl = el.childNodes[el.childNodes.length-1];
+        if (lastEl.nodeType != document.TEXT_NODE) {
+          console.warn("Got command that deletes something that isn't text", command, lastEl);
+          continue;
+        } else {
+          el.removeChild(lastEl);
+        }
+      } else {
+        console.warn("Tried to delete_last_text of element with no children", command);
+        continue;
+      }
+    }
+    if (name === 'replace_text') {
+      if (el.childNodes.length !== 1 || el.childNodes[0].nodeType !== document.TEXT_NODE) {
+        while (el.childNodes) {
+          el.removeChild(el.childNodes[0]);
+        }
+        el.appendChild(document.createTextNode(command[2]));
+      } else {
+        el.childNodes[0].nodeValue = command[2];
+      }
+    }
+    if (name === 'insert_before') {
+      pushes = command[2];
+      for (j=pushes.length-1; j>=0; j--) {
+        if (typeof pushes[j] == 'string') {
+          child = document.createTextNode(pushes[j]);
+        } else {
+          child = Freeze.deserializeElement(pushes[j]);
+        }
+        el.parentNode.insertBefore(child, el);
+      }
+    }
+    if (name === 'append_to') {
+      pushes = command[2];
+      for (j=0; j<pushes.length; j++) {
+        if (typeof pushes[j] == 'string') {
+          child = document.createTextNode(pushes[j]);
+        } else {
+          child = Freeze.deserializeElement(pushes[j]);
+        }
+        el.appendChild(child);
+      }
+    }
+  }
+};
+
+
+Freeze.unfreeze = function (el, serialized) {
+  /* Takes an element and changes it to match the serialized (JSON) version of
+     that element */
+  var tagName = serialized[0];
+  var jsmirrorId = serialized[1];
+  var attrs = serialized[2];
+  var children = serialized[3];
+  if (el.tagName != tagName) {
+    // Heck with it, recreate the element entirely
+    el.parentNode.replaceChild(Freeze.deserializeElement(serialized), el);
+    return;
+  }
+  Freeze.setAttributes(el, attrs);
+  el.jsmirrorId = jsmirrorId;
+  var offset = 0;
+  for (var i=0; i<children.length; i++) {
+    var childIndex = i + offset;
+    var existing = el.childNodes[childIndex];
+    if (! existing) {
+      el.appendChild(Freeze.deserializeElement(children[i]));
+    } else if (existing.jsmirrorHide || existing.id == 'webSocketContainer') {
+      offset++;
+      i--;
+      continue;
+    } else if (typeof children[i] == 'string') {
+      if (existing.nodeType != document.TEXT_NODE) {
+        existing.parentNode.replaceChild(document.createTextNode(children[i]), existing);
+      } else {
+        existing.nodeValue = children[i];
+      }
+    } else {
+      Freeze.unfreeze(existing, children[i]);
+    }
+  }
+  while (el.childNodes.length - offset > children.length) {
+    var node = el.childNodes[children.length + offset];
+    if (node.jsmirrorHide || node.id == 'webSocketContainer') {
+      offset++;
+      continue;
+    }
+    el.removeChild(node);
+  }
+};
+
+Freeze.deserializeElement = function (data) {
+  /* Creates an element to match the given data */
+  if (typeof data == 'string') {
+    return document.createTextNode(data);
+  }
+  var tagName = data[0];
+  var jsmirrorId = data[1];
+  var attrs = data[2];
+  var children = data[3];
+  var el;
+  var text;
+  var i;
+  if (tagName == '<!--COMMENT-->') {
+    if (children && children.length) {
+      text = children[0];
+    } else {
+      text = "";
+    }
+    el = document.createComment(text);
+    el.jsmirrorId = jsmirrorId;
+    return el;
+  }
+  el = document.createElement(tagName);
+  try {
+    for (i in attrs) {
+      if (attrs.hasOwnProperty(i)) {
+        el.setAttribute(i, attrs[i]);
+      }
+    }
+  } catch (e) {
+    console.warn('bad attrs', attrs, JSON.stringify(attrs));
+  }
+  if (children === undefined) {
+    console.warn('Bad children', data);
+    throw 'Bad children list';
+  }
+  for (i=0; i<children.length; i++) {
+    var o = children[i];
+    if (typeof o == "string") {
+      el.appendChild(document.createTextNode(o));
+    } else {
+      el.appendChild(Freeze.deserializeElement(o));
+    }
+  }
+  el.jsmirrorId = jsmirrorId;
+  return el;
+};
+
+Freeze.setAttributes = function (el, attrs) {
+  /* Makes an element's attributes match the given JSON attributes */
+  var attrLength = 0;
+  for (var i in attrs) {
+    if (! attrs.hasOwnProperty(i)) {
+      continue;
+    }
+    attrLength++;
+    el.setAttribute(i, attrs[i]);
+    if (i == 'value') {
+      el.value = attrs[i];
+    }
+  }
+  if (! el.attributes) {
+    console.trace();
+    console.log('el.attributes?', el, el.tagName);
+  }
+  if (el.attributes.length > attrLength) {
+    // There must be an extra attribute to be deleted
+    var toDelete = [];
+    for (i=0; i<el.attributes.length; i++) {
+      if (! attrs.hasOwnProperty(el.attributes[i].name)) {
+        toDelete.push(el.attributes[i].name);
+      }
+    }
+    for (i=0; i<toDelete.length; i++) {
+      console.debug('removing attr', toDelete[i]);
+      el.removeAttribute(toDelete[i]);
+    }
+  }
+};
+
+Freeze.setBase = function (baseHref) {
+  /* Sets the <base href> of the document */
+  var existing = document.getElementsByTagName('base');
+  for (var i=0; i<existing.length; i++) {
+    existing[i].parentNode.removeChild(existing[i]);
+  }
+  var base = document.createElement('base');
+  base.href = baseHref;
+  document.head.appendChild(base);
+};
+
+Freeze._elements = {};
+
+Freeze.getElement = function (id) {
+  return Freeze._elements[id];
+};
+
+Freeze.refreshElements = function () {
+  Freeze._elements = {};
+  function recur(elements, el) {
+    elements[el.jsmirrorId] = el;
+    for (var i=0; i<el.childNodes.length; i++) {
+      var child = el.childNodes[i];
+      if (child.nodeType === document.ELEMENT_NODE) {
+        recur(elements, child);
+      }
+    }
+  }
+  recur(Freeze._elements, document.head);
+  recur(Freeze._elements, document.body);
 };
